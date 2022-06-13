25e440a72e4135fca242f87179fae4a27a82d018
jukkyni <87795531+jukkyni@users.noreply.github.com>
2022-06-14 01:04:53 +0900

    Add files via upload

--- save.py
+++ save.py
@@ -16,7 +16,7 @@ shutil.rmtree(path=unzipedFilePath, ignore_errors=True)
 
 rank = input("第何回(2桁で入力してください 例: 09)\n: ")
 which = input("練習問題を圧縮する場合は任意のキーを,\n課題ファイルを圧縮する場合はそのままreturnを入力してください\n: ")
-targetFiles = f"{rank}_??_*.c" if which != "" else f"{rank}_*_[k,K]22123.c"
+targetFiles = f"{rank}_??_*.c" if which != "" else f"{rank}_*_{studentId}.c"
 with ZipFile(file=zipFilePath, mode="w", compression=ZIP_DEFLATED) as zf:
     for file in glob(targetFiles):
         zf.write(file)
